f0974a34efa5afacdca7db575de3b5d1dd2ab326
Vishnya <vishnyaostap88@gmail.com>
2019-05-17 16:09:33 +0300

    change 5_kuy.js

--- tasks/5_kyu.js
+++ tasks/5_kyu.js
@@ -1,8 +1,8 @@
 
 
 
-// var abcLowerCase ='abcdefghijklmnopqrstuvwxyz'.split('');
-// var abcUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+var abcLowerCase ='abcdefghijklmnopqrstuvwxyz'.split('');
+var abcUpperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
 define(function () {
     return {
