35c45e85b30dcb4cd97eaf823ca1aedbde7adc4e
Nadiia Chorna <nadijagopko@gmail.com>
2019-05-29 00:13:38 +0300

    changed main.js

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 require("amd-loader");
-var msg = require('./tasks/template');
-msg.print("run!");
+//var msg = require('./tasks/template');
+//msg.print("run!");
+
+var kyu8=require('./tasks/8_kyu');
+console.log(kyu8.fixTheMeerkat(['tail', 'body', 'head']));
+console.log(kyu8.squareOrSquareRoot([4,3,9,7,2,1]));
+
+var kyu7=require('./tasks/7_kyu');
+console.log(kyu7.replicate(3,5));
+
+//var kyu6=require('./tasks/6_kyu');
+
+var kyu5=require('./tasks/5_kyu');
+console.log(kyu5.zeros(6));
