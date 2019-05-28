ee637aa69469ec3cfed406330bbee7c9f9f88d77
Nadiia Chorna <nadijagopko@gmail.com>
2019-05-28 18:07:33 +0300

    add tasks solutions

---
+++ tasks/5_kyu.js
@@ -0,0 +1,9 @@
+function zeros (n) {
+ 	var i = 1;
+	var result = 0;
+		while(n/Math.pow(5,i)>1){
+			result += Math.floor(n/Math.pow(5,i));
+			i++;
+		}
+	return result;
+}
---
+++ tasks/8_kyu.js
@@ -0,0 +1,14 @@
+
+// -------------- 1 ----------------
+
+function fixTheMeerkat(arr) {
+ return arr.reverse();
+}
+
+// -------------- 2 ----------------
+
+function squareOrSquareRoot(arr) {
+	return array.map((el)=>{
+		return Number.isInteger(Math.sqrt(el)) ? Math.sqrt(el) : Math.pow(el,2);
+	})
+}
