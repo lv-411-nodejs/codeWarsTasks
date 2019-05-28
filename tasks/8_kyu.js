d637dc0f354884525fe154b114747bf5e81ddd6d
Nadiia Chorna <nadijagopko@gmail.com>
2019-05-29 00:24:27 +0300

    changed param name

--- tasks/8_kyu.js
+++ tasks/8_kyu.js
@@ -14,7 +14,7 @@ define(function(){
 define(function(){
 	return {
 		squareOrSquareRoot: function(arr) {
-			return array.map((el)=>{
+			return arr.map((el)=>{
 				return Number.isInteger(Math.sqrt(el)) ? Math.sqrt(el) : Math.pow(el,2);
 			})
 		}
