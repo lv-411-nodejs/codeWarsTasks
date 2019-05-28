a4e8622948eed53ecec55c5ba46f44e1802332da
Nadiia Chorna <nadijagopko@gmail.com>
2019-05-29 00:29:33 +0300

    changed task 6_kyu

--- tasks/6_kyu.js
+++ tasks/6_kyu.js
@@ -1,14 +1,14 @@
-r = 'Los Angeles Clippers 104 Dallas Mavericks 88,New York Knicks 101 Atlanta Hawks 112,Indiana Pacers 103 Memphis Grizzlies 112, Los Angeles Clippers 100 Boston Celtics 120';
+// r = 'Los Angeles Clippers 104 Dallas Mavericks 88,New York Knicks 101 Atlanta Hawks 112,Indiana Pacers 103 Memphis Grizzlies 112, Los Angeles Clippers 100 Boston Celtics 120';
 
-function nba_cup(str, team){
-let w, d, l, scored, conceded, points;
+// function nba_cup(str, team){
+// let w, d, l, scored, conceded, points;
 
-let arr = str.split(',')
-.map((item)=>{
-return item.match(/([a-zA-Z\s]+)([0-9]+)/ig);
-})
+// let arr = str.split(',')
+// .map((item)=>{
+// return item.match(/([a-zA-Z\s]+)([0-9]+)/ig);
+// })
 
-console.log(arr);
-}
+// console.log(arr);
+// }
 
-nba_cup(r);
+// nba_cup(r);
