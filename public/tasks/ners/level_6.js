81b0769ecc3487c6ca8e0dbc7dc3b68deea55ba3
Ruslan <kick09111@gmail.com>
2019-05-29 15:59:13 +0300

    add my tasks

---
+++ public/tasks/ners/level_6.js
@@ -0,0 +1,13 @@
+module.exports = {
+    bouncingBall: (h,  bounce,  window) => {
+        if(h > 0 && bounce > 0 && bounce < 1) {
+          let count = 0;
+          while(h > window) {
+            h *= bounce;
+            count += 2;
+          }
+          return count - 1;
+        }
+        return -1;
+    }
+}
