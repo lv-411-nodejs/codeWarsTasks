0f7542ddb959a40aad6b930321e9f61753b6b100
Nadiia Chorna <nadijagopko@gmail.com>
2019-05-29 23:30:35 +0300

    added "Ranking NBA teams" task

--- tasks/6_kyu.js
+++ tasks/6_kyu.js
@@ -1,14 +1,31 @@
-// r = 'Los Angeles Clippers 104 Dallas Mavericks 88,New York Knicks 101 Atlanta Hawks 112,Indiana Pacers 103 Memphis Grizzlies 112, Los Angeles Clippers 100 Boston Celtics 120';
+define(function() {
+	return {
+		nba_cup: function(report, teamName) {
+			var w = 0;
+			var d = 0;
+			var l = 0;
+			var score = 0;
+			var conceded = 0;
+			var points = 0;
+			let reg = new RegExp('\\b' + teamName + '\\b', 'ig');
+			let games = report.split(',').filter((item) => {
+				return item.match(reg);
+			}).map((item) => {
+				return item.trim().split(/\s*(\d+\.?\d*\b)\s*/).slice(0, -1);
+			});
+			if (games.length < 1) {
+				return `${team}:This team didn't play!`
+			}
+			let sortgames = games.map((arr) => {
+				return arr.indexOf(teamName) == 0 ? arr : arr = [arr[2], arr[3], arr[0], arr[1]];
+			});
+			sortgames.forEach((game) => {
+				+game[1] > +game[3] ? w += 1 : (+game[1] < +game[3] ? l += 1 : d += 1)
+				score += +game[1];
+				conceded += +game[3];
 
-// function nba_cup(str, team){
-// let w, d, l, scored, conceded, points;
-
-// let arr = str.split(',')
-// .map((item)=>{
-// return item.match(/([a-zA-Z\s]+)([0-9]+)/ig);
-// })
-
-// console.log(arr);
-// }
-
-// nba_cup(r);
+			});
+			return `${teamName}:W=${w};D=${d};L=${l};Scored=${score};Conceded=${conceded};Points=${w*3+d}`;
+		}
+	}
+});
