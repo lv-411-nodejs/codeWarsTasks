0dfba21c3f1536e41ea38f563722e507a35d8680
Домьянич Алексей <alexshadow55@gmail.com>
2019-06-04 11:14:59 +0300

    fix 5kyu

--- server/controllers/kyu5.js
+++ server/controllers/kyu5.js
@@ -62,23 +62,22 @@ module.exports = {
     } = req.body;
 
     const smallest = (n) => {
-      const row = String(n).split('');
+      let row = String(n).split("");
       let min = [n, 0, 0];
       let test = [];
 
       for (let i = 0, length = row.length; i < length; i++) {
         for (let j = 0; j < length; j++) {
           test = row.slice(0, i).concat(row.slice(i + 1));
-          const check = Number(test.slice(0, j).concat(row[i], test.slice(j))
-            .join('')) < min[0];
-          if (check) {
-            min = [check, i, j];
+
+          if (Number(test.slice(0, j).concat(row[i], test.slice(j)).join("")) < min[0]) {
+            min = [Number(test.slice(0, j).concat(row[i], test.slice(j)).join("")), i, j];
           }
         }
       }
 
       return min;
-    };
+    }
 
     try {
       const validatorArg = new validator([n]);
